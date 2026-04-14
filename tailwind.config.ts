f314a1c5d276df16fbf6f9a00eb0cf7676d4116e
xingchendahai666 <3436483129@qq.com>
2026-04-14 12:29:54 +0800

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,7 @@
+import type { Config } from "tailwindcss";
+const config: Config = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./lib/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: { colors: { slate: { 950: '#020617' } } } },
+  plugins: [],
+};
+export default config;
